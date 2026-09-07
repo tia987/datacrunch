ebb98a0ab77291e85089cf3b743e6e6753fc9b83
tia987 <mlucca@ethz.ch>
2026-09-07 10:18:46 +0200

    Initial commit

---
+++ installation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install required packages (source ./installation.sh)
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
