29b385085d3412945016115ac30a434b68760510
tia987 <mlucca@ethz.ch>
2026-09-07 16:13:51 +0200

    test small resources

---
+++ euler.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=AML25            # Job name    (default: sbatch)
+#SBATCH --output=aml.out            # Output file (default: slurm-%j.out)
+#SBATCH --error=aml.err             # Error file  (default: slurm-%j.out)
+#SBATCH --nodes=1                   # Number of nodes
+#SBATCH --ntasks=1                  # Number of tasks
+#SBATCH --ntasks-per-node=1         # Number of tasks per node
+#SBATCH --constraint=EPYC_7763      # Select node with CPU
+#SBATCH --mem-per-cpu=1024          # Memory per CPU
+#SBATCH --time=24:00:00             # Wall clock time limit
+#SBATCH --mail-type=END,FAIL        # Send an email when job ends
+
+# Load some modules
+module load stack/2025-06 gcc/12.2.0 python/3.13.0 eth_proxy
+module list
+
+# Load environment
+source installation.sh
+
+# Run the program
+# python3 main.py -val=0 --save_params=1 --load_params=0
+jupyter nbconvert --execute datacrunch.ipynb
