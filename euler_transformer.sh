1f7ab9c9e7f155da2fd27e000f554a36fb097d9b
tia987 <mlucca@ethz.ch>
2026-09-08 10:35:42 +0200

    Revert

--- euler_transformer.sh
+++ euler_transformer.sh
@@ -3,10 +3,10 @@
 #SBATCH --output=TRA.out            # Output file (default: slurm-%j.out)
 #SBATCH --error=TRA.err             # Error file  (default: slurm-%j.out)
 #SBATCH --nodes=1                   # Number of nodes
-#SBATCH --ntasks=48                 # Number of tasks
-#SBATCH --ntasks-per-node=48         # Number of tasks per node
+#SBATCH --ntasks=1                  # Number of tasks
+#SBATCH --ntasks-per-node=1         # Number of tasks per node
 #SBATCH --constraint=EPYC_7763      # Select node with CPU
-#SBATCH --mem=70G                   # Memory per CPU
+#SBATCH --mem-per-cpu=70G           # Memory per CPU
 #SBATCH --time=36:00:00             # Wall clock time limit
 #SBATCH --mail-type=END,FAIL        # Send an email when job ends
 
