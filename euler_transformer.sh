05813d6d0a5d5a2da9d28fdf4ab95d20876192a2
tia987 <mlucca@ethz.ch>
2026-09-08 10:20:08 +0200

    More ram

--- euler_transformer.sh
+++ euler_transformer.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1                  # Number of tasks
 #SBATCH --ntasks-per-node=1         # Number of tasks per node
 #SBATCH --constraint=EPYC_7763      # Select node with CPU
-#SBATCH --mem-per-cpu=55000         # Memory per CPU
+#SBATCH --mem-per-cpu=64000         # Memory per CPU
 #SBATCH --time=36:00:00             # Wall clock time limit
 #SBATCH --mail-type=END,FAIL        # Send an email when job ends
 
