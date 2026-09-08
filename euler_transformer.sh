e3a1bd6fc8f2709959e73939b1cda868ab5f308c
tia987 <mlucca@ethz.ch>
2026-09-08 10:34:53 +0200

    1 Node only

--- euler_transformer.sh
+++ euler_transformer.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=TRA25            # Job name    (default: sbatch)
 #SBATCH --output=TRA.out            # Output file (default: slurm-%j.out)
 #SBATCH --error=TRA.err             # Error file  (default: slurm-%j.out)
-#SBATCH --nodes=8                   # Number of nodes
+#SBATCH --nodes=1                   # Number of nodes
 #SBATCH --ntasks=48                 # Number of tasks
-#SBATCH --ntasks-per-node=6         # Number of tasks per node
+#SBATCH --ntasks-per-node=48         # Number of tasks per node
 #SBATCH --constraint=EPYC_7763      # Select node with CPU
 #SBATCH --mem=70G                   # Memory per CPU
 #SBATCH --time=36:00:00             # Wall clock time limit
