f3545087853e1a603f5eb208834edf60009ac247
tia987 <mlucca@ethz.ch>
2026-09-09 13:15:01 +0200

    Max ram

--- euler.sh
+++ euler.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1                  # Number of tasks
 #SBATCH --ntasks-per-node=1         # Number of tasks per node
 #SBATCH --cpus-per-task=16          # Number of tasks per task
-#SBATCH --mem-per-cpu=4G            # Memory per CPU
+#SBATCH --mem-per-cpu=8G            # Memory per CPU
 #SBATCH --time=36:00:00             # Wall clock time limit
 #SBATCH --mail-type=END,FAIL        # Send an email when job ends
 
