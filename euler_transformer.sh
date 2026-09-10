8684705bb39b47571c9955f320195beff3d2672c
tia987 <mlucca@ethz.ch>
2026-09-10 09:45:20 +0200

    changed name

--- euler_transformer.sh
+++ euler_transformer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=TRA25            # Job name    (default: sbatch)
-#SBATCH --output=TRA.out            # Output file (default: slurm-%j.out)
-#SBATCH --error=TRA.err             # Error file  (default: slurm-%j.out)
+#SBATCH --output=tra.out            # Output file (default: slurm-%j.out)
+#SBATCH --error=tra.err             # Error file  (default: slurm-%j.out)
 #SBATCH --nodes=1                   # Number of nodes
 #SBATCH --ntasks=1                  # Number of tasks
 #SBATCH --ntasks-per-node=1         # Number of tasks per node
